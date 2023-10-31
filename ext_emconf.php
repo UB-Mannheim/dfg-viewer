74b710009a67757107f77aaaa3744e23898b6757
Bernd Fallert <bernd.fallert@uni-mannheim.de>
2023-10-31 14:07:00 +0100

    dlf @dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,25 +1,25 @@
 <?php
 /**
- * Copyright notice
- *
+*  Copyright notice
+*
  * (c) Saxon State and University Library Dresden <typo3@slub-dresden.de>
- * All rights reserved
- *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
  */
 
 $EM_CONF[$_EXTKEY] = [
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = [
     'shy' => '',
     'priority' => '',
     'module' => '',
-    'state' => 'stable',
+    'state' => 'dev',
     'internal' => '',
     'uploadfolder' => false,
     'createDirs' => '',
@@ -52,6 +52,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'php' => '8.1.0-8.3.99',
             'typo3' => '11.5.0-12.4.99'
+            'dlf' => '@dev',
         ],
         'conflicts' => [
         ],
