982038a699f596c684ff848f64b860bf98ac4fcc
BFallert <bernd.fallert@bib.uni-mannheim.de>
2026-02-10 13:30:42 +0100

    fix ,

--- ext_emconf.php
+++ ext_emconf.php
@@ -51,8 +51,8 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.1.0-8.4.99',
-            'typo3' => '11.5.0-12.4.99'
-            'dlf' => '@dev',
+            'typo3' => '11.5.0-12.4.99',
+            'dlf' => '@dev'
         ],
         'conflicts' => [
         ],
