a3c578256d4e07aa3b04d084af4dc834131838ae
Bernd Fallert <bernd.fallert@uni-mannheim.de>
2026-03-20 07:44:31 +0100

    fix: close submenus when clicking another submenu or item

--- Resources/Private/JavaScript/dfgviewerScripts.js
+++ Resources/Private/JavaScript/dfgviewerScripts.js
@@ -47,6 +47,9 @@ $(document).ready(function() {
 
     // menu toggles for offcanvas toc and metadata
     $('.offcanvas-toggle').on(mobileEvent, function(event) {
+        // close nav on link or download if opend
+        close_all_submenues();
+
         $(this).parent().toggleClass('open');
     });
 
@@ -56,6 +59,12 @@ $(document).ready(function() {
 
     // active toggle for submenus
     $('.document-functions li.submenu > a').on(mobileEvent, function(event) {
+        // close nav on link or download if opend
+        close_all_submenues('in-secondary-nav');
+
+        // close secondary nav if click on link or download
+        $('nav .secondary-nav').removeClass('open');
+
         $('li.submenu.open a').not(this).parent().removeClass('open');
         $(this).parent().toggleClass('open');
         return false;
@@ -120,6 +129,9 @@ $(document).ready(function() {
     $('nav .nav-toggle').on(mobileEvent, function(event) {
         $(this).toggleClass('active');
         $('nav .viewer-nav').toggleClass('open');
+
+        // close subnav when primary nav if open
+        close_all_submenues('in-primary-nav');
     });
 
     // calendar dropdowns
@@ -189,7 +201,8 @@ $(document).ready(function() {
     }
 
     // enable click on fullscreen button
-    $('li.tx-dlf-tools-fullscreen a').on(mobileEvent, function() {
+$('a.fullscreen, li.tx-dlf-tools-fullscreen a').on(mobileEvent, function() {
+        close_all_submenues('all');
         if($('body.fullscreen')[0]) {
             exitFullscreen();
         } else {
@@ -265,6 +278,17 @@ $(document).ready(function() {
         $('body').removeClass('static');
     }, 1000);
 
+    // Closing open menus in different situations
+    $('.tx-dlf-tools-imagetools').on('click', function (event) {
+        close_all_submenues('all');
+    });
+    $('.page-control').on('click', function (event) {
+        close_all_submenues('all');
+    });
+    $('.tx-dlf-map').on('click', function (event) {
+        close_all_submenues('all');
+    });
+
 });
 
 $(document).keyup(function(e) {
@@ -303,4 +327,18 @@ function exitFullscreen() {
 
 function showLoadingAnimation() {
     $("#overlay").fadeIn(300);
+}
+
+function close_all_submenues(environment = '') {
+    // close nav on link or download if opened
+    if (environment !== 'in-secondary-nav') {
+        // Not with in-seondary-nav otherwise menus can no longer be closed
+        $('li.submenu.open a').parent().removeClass('open');
+    };
+    if ((environment === 'in-secondary-nav') || (environment === 'all') ) {
+        // close subnav if opend
+        $('nav .nav-toggle').removeClass('active');
+        $('nav .secondary-nav').removeClass('open');
+        $('nav ul.viewer-nav').removeClass('open');
+    };
 }
